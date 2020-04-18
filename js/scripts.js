22f76511b5af3352033f44e27425535e3d014876
P17153048 <P17153048>
2020-04-18 11:37:14 +0530

    Unique alt for images in gallery

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,52 @@ function displayGallery() {
     document.getElementById(
       `gallery-images`
     ).innerHTML += `<div class='col-4 gallery'>
-        <img class='gallery-img' src='assets/images/trainers/trainer_${i}.jpg' alt='Trainer'>
+        <img class='gallery-img' src='assets/images/trainers/trainer_${i}.jpg' alt='Trainer-${i}'>
       </div>`;
   }
 }
 
-document.addEventListener(`DOMContentLoaded`, function (event) {
+document.addEventListener(`DOMContentLoaded`, (event) => {
   displayGallery();
 });
+
+document.getElementById("contact-form").addEventListener("submit", () => {
+  if (!validateForm()) {
+    alert("Invalid phone number");
+    return;
+  }
+  sendMessage();
+});
+
+function validateForm() {
+  let phone = getValueById("phone");
+  if (!phone) {
+    return false;
+  }
+  if (isNaN(phone)) {
+    return false;
+  }
+  return true;
+}
+
+const getValueById = (id) => {
+  return document.getElementById(id).value;
+};
+
+const sendMessage = () => {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      alert("Message sent successfully");
+    }
+  };
+  xhttp.open("POST", "https://jsonplaceholder.typicode.com/posts", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send(
+    `fname=${getValueById("fname")}&lname=${getValueById(
+      "lname"
+    )}&email=${getValueById("email")}&phone=${getValueById(
+      "phone"
+    )}&subject=${getValueById("subject")}&message=${getValueById("message")}`
+  );
+};
